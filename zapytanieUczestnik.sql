07c7e3ffd76bc4b20cf9888cef204a2e795d7566
Patrycja Jaworek <01169293@pw.edu.pl>
2024-04-16 18:23:46 +0200

    Dodanie projektu wraz z plikami

--- zapytanieUczestnik.sql
+++
@@ -1,9 +0,0 @@
-SELECT [idUczestnika]
-      ,[imieUczestnika]
-      ,[nazwiskoUczestnika]
-      ,[emailUczestnika]
-      ,[dataUrodzenia]
-      ,[dataUtworzenia]
-      ,[dataModyfikacji]
-  FROM [dbo].[UczestnicyAkcji]
-  WHERE [dataModyfikacji] >= dateadd(day, -1, cast(CURRENT_TIMESTAMP as date))
